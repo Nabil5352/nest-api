72af979164ea65eb7699257ca06bd3681d8317bb
Nabil Ahmad <report.nabil@gmail.com>
2023-06-14 15:45:10 +0600

    added initial skeleton

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{
+    signup() {
+        message: 'I am signup'
+    }
+
+    signin() {
+        message: 'I am signup'
+    }
+}
